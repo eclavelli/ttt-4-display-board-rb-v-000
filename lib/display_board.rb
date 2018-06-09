93ddf970db5bf5100d3d2b48706bfda160fd40cc
Elina Clavelli <elina.clavelli@me.com>
2018-06-09 03:20:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["x"," "," "," "," "," "," "," "," "]
 
 display_board(board)
+
